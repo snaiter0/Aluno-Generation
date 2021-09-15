9b23a206a2274c77b9e037545bb0c9659e0d7103
snaiter0 <Douglas.szlps@gmail.com>
2021-09-14 21:54:02 -0300

    [Feature Doug - Task: Deploy BlogPessoal]

--- SpingBoot/BlogPessoal/src/main/java/org/generation/BlogPessoal/Seguranca/BasicSecurityConfig.java
+++ SpingBoot/BlogPessoal/src/main/java/org/generation/BlogPessoal/Seguranca/BasicSecurityConfig.java
@@ -2,6 +2,7 @@ package org.generation.BlogPessoal.Seguranca;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,30 +19,36 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter
 	private UserDetailsService userDetailsService;
 	
 	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception
-	{
-		auth.userDetailsService(userDetailsService);
+	protected void configure(AuthenticationManagerBuilder auth)
+	throws Exception {
+	auth.userDetailsService(userDetailsService);
+	auth.inMemoryAuthentication()
+	.withUser("root")
+	.password(PasswordEncoder().encode("root"))
+	.authorities("ROLE_USER");
 	}
+	
 	@Bean
 	public PasswordEncoder PasswordEncoder()
 	{
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-		.antMatchers("/usuarios/logar").permitAll()
-		.antMatchers("/usuarios/cadastrar").permitAll()	
-		.antMatchers("/Postagens/home").permitAll()	
-		.anyRequest().authenticated()
-		.and().httpBasic()
-		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-		.and().cors()
-		.and().csrf().disable();
-		
-		
+	http.authorizeRequests()
+	.antMatchers("/usuarios/cadastrar").permitAll()
+	.antMatchers("/usuarios/logar").permitAll()
+	.antMatchers(HttpMethod.OPTIONS).permitAll()
+	.anyRequest().authenticated()
+	.and().httpBasic()
+	.and().sessionManagement()
+	.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+	.and().cors()
+	.and().csrf().disable();
 	}
 	
+	
+
 }
 
--- SpingBoot/BlogPessoal/src/test/java/org/generation/BlogPessoal/BlogPessoalApplicationTests.java
+++ SpingBoot/BlogPessoal/src/test/java/org/generation/BlogPessoal/BlogPessoalApplicationTests.java
@@ -2,6 +2,9 @@ package org.generation.BlogPessoal;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootTest
 class BlogPessoalApplicationTests {
